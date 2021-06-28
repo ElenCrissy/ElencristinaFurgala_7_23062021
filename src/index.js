f2af0a51ce28d86e72485eb2d5047ee1988a2485
Elencrissy <cristina.furgala@gmail.com>
2021-06-28 22:48:43 +0200

    Design

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import recipes from './recipes.js';
+
+
+createCategory(categoryName) {
+    const category = document.createElement('div');
+    const categoryForm = document.createElement('form');
+    const categoryInput = document.createElement('input');
+
+    category.classList.add('category');
+    categoryForm.setAttribute('method', 'get');
+    categoryForm.setAttribute('action', 'traitement.php');
+    categoryInput.setAttribute('type', 'text');
+    categoryInput.setAttribute('name', 'search');
+    categoryInput.setAttribute('placeholder', `${category}`);
+    categoryInput.setAttribute('id', `search-by-${category}`);
+
+    if(category === Ingrédients) {
+        category.style.backgroundColor = $colour-blue;
+    } else if (category === Appareils) {
+        category.style.backgroundColor = $colour-green;
+    } else {
+        category.style.backgroundColor = $colour-red;
+    }
+
+    categoryForm.appendChild(categoryInput);
+    category.appendChild(categoryForm);
+}
+
+generateCategories(container) {
+    let categories = ['Ingrédients', 'Appareils', 'Ustensiles'];
+    categories.forEach(category => container.appendChild(createCategory(category)));
+}
+
+
 createRecipeCard(container) {
     const card = document.createElement('div');
     const greyBlock = document.createElement('div');
@@ -9,8 +43,4 @@ createRecipeCard(container) {
 
     card.append(greyBlock, recipe);
     container.appendChild(card);
-}
-
-generateCards() {
-    
 }
