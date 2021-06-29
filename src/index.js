202489c1ddf42bd91ea7d73c53c72a184b8f837b
Elencrissy <cristina.furgala@gmail.com>
2021-06-29 12:34:50 +0200

    Design

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
-import recipes from './recipes.js';
+// import {recipes} from '../recipes.js';
 
+// const cardsSection = document.querySelector('.cards-section');
+// console.log(recipesArray);
 
-createCategory(categoryName) {
-    const category = document.createElement('div');
-    const categoryForm = document.createElement('form');
-    const categoryInput = document.createElement('input');
+// createCategory(categoryName) {
+//     const category = document.createElement('div');
+//     const categoryForm = document.createElement('form');
+//     const categoryInput = document.createElement('input');
 
-    category.classList.add('category');
-    categoryForm.setAttribute('method', 'get');
-    categoryForm.setAttribute('action', 'traitement.php');
-    categoryInput.setAttribute('type', 'text');
-    categoryInput.setAttribute('name', 'search');
-    categoryInput.setAttribute('placeholder', `${category}`);
-    categoryInput.setAttribute('id', `search-by-${category}`);
+//     category.classList.add('category');
+//     categoryForm.setAttribute('method', 'get');
+//     categoryForm.setAttribute('action', 'traitement.php');
+//     categoryInput.setAttribute('type', 'text');
+//     categoryInput.setAttribute('name', 'search');
+//     categoryInput.setAttribute('placeholder', `${category}`);
+//     categoryInput.setAttribute('id', `search-by-${category}`);
 
-    if(category === Ingrédients) {
-        category.style.backgroundColor = $colour-blue;
-    } else if (category === Appareils) {
-        category.style.backgroundColor = $colour-green;
-    } else {
-        category.style.backgroundColor = $colour-red;
-    }
+//     if(category.toString() === 'Ingrédients') {
+//         category.style.backgroundColor = $colour-blue;
+//     } else if (category.toString() === 'Appareils') {
+//         category.style.backgroundColor = $colour-green;
+//     } else {
+//         category.style.backgroundColor = $colour-red;
+//     }
 
-    categoryForm.appendChild(categoryInput);
-    category.appendChild(categoryForm);
-}
+//     categoryForm.appendChild(categoryInput);
+//     category.appendChild(categoryForm);
+// };
 
-generateCategories(container) {
-    let categories = ['Ingrédients', 'Appareils', 'Ustensiles'];
-    categories.forEach(category => container.appendChild(createCategory(category)));
-}
+// generateCategories(container) {
+//     let categories = ['Ingrédients', 'Appareils', 'Ustensiles'];
+//     categories.forEach(category => container.appendChild(createCategory(category)));
+// };
 
+// createRecipeCard(){
+//     const card = document.createElement('div');
+//     const cardFigure = document.createElement('figure');
+//     const cardImg = document.createElement('img');
+//     const cardCaption = document.createElement('figcaption');
 
-createRecipeCard(container) {
-    const card = document.createElement('div');
-    const greyBlock = document.createElement('div');
-    const recipe = document.createElement('div');
+//     card.classList.add('card');
+//     cardImg.classList.add('card-img');
+//     cardCaption.classList.add('card-caption');
 
-    card.classList.add('card');
-    greyBlock.classList.add('greyBlock');
-    recipe.classList.add('recipe');
-
-    card.append(greyBlock, recipe);
-    container.appendChild(card);
-}
+//     cardFigure.append(cardImg, cardCaption);
+//     card.append(cardFigure);
+//     cardsSection.appendChild(card);
+// };
